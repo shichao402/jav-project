39c3880b8b471b0b4bef9fe56c698b59b60bebc4
shichao402 <shichao402@gmail.com>
2015-12-11 16:51:12 +0800

    7z

--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
 cd %~dp0
 scrapy crawl jav
+set yy = %date:~3,4%
+set mm = %date:~8,2%
+set dd = %date:~11,2%
+"c:\Program Files\7-Zip\7z.exe" a -pjav%yy%%mm%%dd% -mhe jav%yy%%mm%%dd% d:\jav-project\output\%yy%%mm%%dd% -o d:\jav-project\output\
