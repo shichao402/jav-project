c3082ebd58437e9d0e212821a65c5cfbdb2914d6
shichao402 <shichao402@gmail.com>
2017-01-17 13:09:42 +0800

    test

--- jav/settings.py
+++ jav/settings.py
@@ -24,15 +24,15 @@ DUPEFILTER_CLASS = 'scrapy.dupefilters.RFPDupeFilter'
 #USER_AGENT = 'jav (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=3
+CONCURRENT_REQUESTS=2
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY=1
+DOWNLOAD_DELAY=2
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN=3
-CONCURRENT_REQUESTS_PER_IP=3
+CONCURRENT_REQUESTS_PER_DOMAIN=2
+CONCURRENT_REQUESTS_PER_IP=2
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED=False
